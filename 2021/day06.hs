a9903c1e44d134a51285336f701b5c205462b886
Wyatt Allen <wyatt.allen0@gmail.com>
2021-12-25 16:34:24 -0800

    2021 day 6

---
+++ 2021/day06.hs
@@ -0,0 +1,37 @@
+sampleSeed :: [Int]
+sampleSeed = [3,4,3,1,2]
+
+num :: Int -> Int -> [Int]
+num 9 _ = []
+num m n = (if m == n then 1 else 0) : num (succ m) n
+
+addS :: [Int] -> [Int] -> [Int]
+addS = zipWith (+)
+
+mulS :: Int -> [Int] -> [Int]
+mulS n = map (n*)
+
+nextS :: [Int] -> [Int]
+nextS (z:s)
+  = addS (s ++ [0])
+  $ addS (mulS z $ num 0 6)
+  $ mulS z
+  $ num 0 8
+
+seedToState :: [Int] -> [Int]
+seedToState = foldr1 addS . map (num 0)
+
+days :: [Int] -> [[Int]]
+days seed = seed : (days $ nextS seed)
+
+parseFile :: String -> [Int]
+parseFile [] = []
+parseFile (c:',':cs) = read [c] : parseFile cs
+
+main = do
+  f <- readFile "day06.input.txt"
+  let seed = seedToState $ read $ "[" ++ f ++ "]"
+  putStr "Part 1: "
+  print $ sum $ days seed !! 80
+  putStr "Part 2: "
+  print $ sum $ days seed !! 256
